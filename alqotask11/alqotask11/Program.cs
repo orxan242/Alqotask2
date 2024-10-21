ec71f688f1b08306f8128003812d326e0f83bd1c
orxan242 <orkhanka@code.edu.az>
2024-10-21 18:35:09 +0400

    added alqo task

---
+++ alqotask11/alqotask11/Program.cs
@@ -0,0 +1,82 @@
+﻿#region work1
+//int n = 2;
+//int m = 55;
+//for (int i = n; i <= m; i ++)
+//{
+//    if (i%2==1)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+
+#region work2
+//int n = 7;
+//int m = 32;
+//int sum = 0;
+//for (int i = n; i <= m; i ++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+#endregion
+
+#region work3
+//int m = 9;
+//int sum = 1;
+//for (int i = 1; i <= m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        sum *= i;
+//    }
+//}
+//Console.WriteLine(sum);
+#endregion
+
+#region work4
+
+//int a = 15;
+//if (a % 3 == 0)
+//{
+//     if (a % 2 == 0)
+//    {
+//        Console.WriteLine("3e bolunen cutdur");
+//    }
+//    else
+//    {
+//        Console.WriteLine("3e bolunur tekdir");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("3e bolunmur");
+//}
+
+
+#endregion
+
+#region work5
+//int n = 53; 
+//int sum = 0;
+//int  count = 0;
+//for (int i = 2; i < n; i++)
+//{
+//    if (n%i== 0)
+//    {
+//       count++;
+//    }
+//}
+//if (count == 0)
+//{
+//    Console.WriteLine("sade");
+//}
+//else
+//{
+//    Console.WriteLine("murekkeb");
+//}
+#endregion
